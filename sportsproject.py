a5c7d0531c94c3a28b09022d51a8216892604c16
patrickpu1 <133797847+patrickpu1@users.noreply.github.com>
2023-06-06 10:34:51 -0700

    Update sportsproject.py

--- sportsproject.py
+++ sportsproject.py
@@ -4,3 +4,31 @@ class Quiz:
   def __init__(self):
       self.questions = []
       self.score = 0
+  
+  def add_question(self, question, answer):
+    self.questions.append((question,answer))
+    
+  def run_quiz(self):
+    for question, answer in self.questions:
+      user_answer= input(question+" ")
+      if user_answer.lower()== answer.lower():
+        self.score +=1
+        
+  def save_score(self, name):
+    try:
+      with open("high_scores.pkl", "rb") as file:
+        high_scores = pickle.load(file)
+    except FileNotFoundError:
+      high_scores=[]
+      
+    high_scores.append((name, self.score))
+    
+    with open("high_scores.pkl","wb") as file:
+      pickle.dump(high_scores, file)
+  def show_high_scores(self):
+    try:
+      with open("high_scores.pkl", "rb") as file:
+        high_scores=pickle.load(file)
+      except FilNotFoundError:
+        print("no high scores yet.")
+        return
